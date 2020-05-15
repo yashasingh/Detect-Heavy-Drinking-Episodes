a18d84bc88f19653e5283b6f88015c9829f68dd2
Nikhil Doifode <nikhildoifode200@gmail.com>
2020-05-15 18:33:54 -0400

    New Features added: #1  Mean #2  Standard Deviation #3  Median #4  Zero Crossing Rate #5  Max/Min, Raw/Abs #6  Spectral Entropy #7  Spectral Centroid #8  Spectral Spread #10 Spectral Roll-off #11 Max Frequency #16 Skewness #17 Kurtosis #18 Average Power

--- eda_edits_copy.py
+++ eda_edits_copy.py
@@ -4,7 +4,53 @@ import pandas as pd
 from datetime import datetime
 import pickle
 import statistics
+import utility
+from scipy.fftpack import fft
+import enum
+from scipy.stats import kurtosis
 
+class Features(enum.Enum):
+	Mean = 0
+	Median = 1
+	Std_Dev = 2
+	ZeroCrsRate = 3
+	Max_Raw = 4
+	Min_Raw = 5
+	Max_Abs = 6
+	Min_Abs = 7
+	Spec_Ent_Time = 8
+	Spec_Ent_Freq = 9
+	Spec_Cent = 10
+	Spec_Sprd = 11
+	Spec_Flux = 12
+	Spec_RollOff = 13
+
+	Max_freq = 14
+	Skewness = 15
+	Kurtosis = 16
+	Avg_Power = 17
+	RMS = 18
+
+
+FeatureType = {}
+FeatureType[Features.Mean] = 'Mean'
+FeatureType[Features.Median] = 'Median'
+FeatureType[Features.Std_Dev] = 'Std_Dev'
+FeatureType[Features.ZeroCrsRate] = 'ZeroCrsRate'
+FeatureType[Features.Max_Raw] = 'Max_Raw'
+FeatureType[Features.Min_Raw] = 'Min_Raw'
+FeatureType[Features.Max_Abs] = 'Max_Abs'
+FeatureType[Features.Min_Abs] = 'Min_Abs'
+FeatureType[Features.Spec_Ent_Time] = 'Spec_Ent_Time'
+FeatureType[Features.Spec_Ent_Freq] = 'Spec_Ent_Freq'
+FeatureType[Features.Spec_Cent] = 'Spec_Cent'
+FeatureType[Features.Spec_Sprd] = 'Spec_Sprd'
+FeatureType[Features.Spec_Flux] = 'Spec_Flux'
+FeatureType[Features.Spec_RollOff] = 'Spec_RollOff'
+FeatureType[Features.Max_freq] = 'Max_freq'
+FeatureType[Features.Skewness] = 'Skewness'
+FeatureType[Features.Kurtosis] = 'Kurtosis'
+FeatureType[Features.Avg_Power] = 'Avg_Power'
 
 
 def seperate_pid_data():
@@ -15,14 +61,18 @@ def seperate_pid_data():
 		df.to_csv(pid, encoding='utf-8')
 		print(str(pid)+".csv created")
 	print(pids)
+
 	return pids
 
-def create_per_second_data(pid_filename, metric_name):
+
+def create_per_second_data(pid_filename, metric_no):
 	acc_data = pd.read_csv(pid_filename)
 	prev_ts = 0 
 	full_frame = list()
 	sub_frame = list()
 	tot_rows = len(acc_data)
+	# count = 0
+	# fft_magnitude_previous = {}
 
 	for idx in range(0, tot_rows):
 
@@ -41,17 +91,60 @@ def create_per_second_data(pid_filename, metric_name):
 			sub_frame = np.array(sub_frame)
 			metrics_axis = []
 
+			# if count == 0:
+				# fft_magnitude_previous[0] = 0
+				# fft_magnitude_previous[1] = 0
+				# fft_magnitude_previous[2] = 0
+				# count += 1
+
 			# Add the last timstamp in that second window
 			metrics_axis.append(sub_frame[-1, 0])
 
 			# Iterating over x, y, z axis
 			for col in range(1,4):
-				if metric_name == 'Mean':
+				if metric_no == Features.Mean.value:
 					metrics_axis.append(sub_frame[:, col].mean())
-				elif metric_name == 'Std_Dev':
+				elif metric_no == Features.Median.value:
 					metrics_axis.append(statistics.stdev(sub_frame[:, col]))
-				elif metric_name == 'Median':
+				elif metric_no == Features.Std_Dev.value:
 					metrics_axis.append(statistics.median(sub_frame[:, col]))
+				elif metric_no == Features.ZeroCrsRate.value:
+					metrics_axis.append(utility.zero_crossing_rate(sub_frame[:, col]))
+				elif metric_no == Features.Max_Raw.value:
+					metrics_axis.append(max(sub_frame[:, col]))
+				elif metric_no == Features.Min_Raw.value:
+					metrics_axis.append(min(sub_frame[:, col]))
+				elif metric_no == Features.Max_Abs.value:
+					metrics_axis.append(max(abs(sub_frame[:, col])))
+				elif metric_no == Features.Min_Abs.value:
+					metrics_axis.append(min(abs(sub_frame[:, col])))
+				elif metric_no == Features.Spec_Ent_Time.value:
+					metrics_axis.append(utility.spectral_entropy(sub_frame[:, col]))
+				elif metric_no == Features.Spec_Ent_Freq.value:
+					fft_magnitude = abs(fft(sub_frame[:, col]).real)
+					metrics_axis.append(utility.spectral_entropy(fft_magnitude))
+				elif metric_no == Features.Spec_Cent.value:
+					fft_magnitude = abs(fft(sub_frame[:, col]).real)
+					metrics_axis.append(utility.spectral_centroid(fft_magnitude))
+				elif metric_no == Features.Spec_Sprd.value:
+					fft_magnitude = abs(fft(sub_frame[:, col]).real)
+					metrics_axis.append(utility.spectral_spread(fft_magnitude))
+				# elif metric_no == Features.Spec_Flux.value:
+					# fft_magnitude = abs(fft(sub_frame[:, col]).real)
+					# metrics_axis.append(utility.spectral_flux(fft_magnitude, fft_magnitude_previous[col-1]))
+					# fft_magnitude_previous[col-1] = fft_magnitude.copy()
+				elif metric_no == Features.Spec_RollOff.value:
+					fft_magnitude = abs(fft(sub_frame[:, col]).real)
+					metrics_axis.append(utility.spectral_rolloff(fft_magnitude))
+				elif metric_no == Features.Max_freq.value:
+					fft_magnitude = abs(fft(sub_frame[:, col]).real)
+					metrics_axis.append(max(fft_magnitude))
+				elif metric_no == Features.Skewness.value:
+					metrics_axis.append(utility.skewness(sub_frame[:, col]))
+				elif metric_no == Features.Kurtosis.value:
+					metrics_axis.append(kurtosis(sub_frame[:, col]))
+				elif metric_no == Features.Avg_Power.value:
+					metrics_axis.append(utility.avg_power(sub_frame[:, col]))
 
 			full_frame.append(metrics_axis)
 			sub_frame = list()
@@ -59,8 +152,7 @@ def create_per_second_data(pid_filename, metric_name):
 	full_frame = np.array(full_frame)
 
 	# Pickling this data
-
-	filename = metric_name+'_all_per_sec_all_axis.pkl'
+	filename = str(str(FeatureType[Features(metric_no)]) + '_all_per_sec_all_axis.pkl')
 	outfile = open(filename,'wb')
 	pickle.dump(full_frame,outfile)
 	outfile.close()
@@ -70,9 +162,7 @@ def create_per_second_data(pid_filename, metric_name):
 
 
 def create_per_window_data(filename, metric_no):
-
 	# Read the pickle file that contains entry for each second
-
 	infile = open(filename,'rb')
 	mean_all = pickle.load(infile)
 	infile.close()
@@ -84,15 +174,11 @@ def create_per_window_data(filename, metric_no):
 	print("Shape of data obatined from pickle - ", mean_all.shape)
 
 	# Calculate summary statistics for this metric
-
 	while i+10 < tot_rows:
 		single_row.append(mean_all[i+9:i+10, 0][0])
 
 		for col in range(1, 4):
-
 			sub_frame = mean_all[i:i+10, col]
-
-
 
 			single_row.append(sub_frame.mean())
 			single_row.append(sub_frame.var())
@@ -105,67 +191,52 @@ def create_per_window_data(filename, metric_no):
 		full_frame.append(single_row)
 		single_row = list()
 		i += 10
-	full_frame = np.array(full_frame)
 
-	
+	full_frame = np.array(full_frame)	
 	print("Shape of generated frame for each 10 sec window ", full_frame.shape)
 
 	col_names = ['xMe', 'xVr', 'xMx', 'xMi', 'xUM', 'xLM', 'yMe', 'yVr', 'yMx', 'yMn', 'yUM', 'yLM', 'zMe', 'zVr', 'zMx', 'zMi', 'zUM', 'zLM']
 
-	df1 = pd.DataFrame.from_records(full_frame, columns = ['t']+[str(metric_no+names) for names in col_names] )
+	df1 = pd.DataFrame.from_records(full_frame, columns = ['t'] + [str(str(metric_no) + names) for names in col_names] )
 	print("df1 created !!!!")
 
-	# Calculating the values out of difference of two windows
+	if (metric_no <= 13):
+		# Calculating the values out of difference of two windows
+		diff_frame = list()
+		for i in range(len(full_frame)):
+			if i==0: diff_frame.append(full_frame[:1, 1:][0])
+			else:
+				diff = full_frame[i:i+1, 1:] - full_frame[i-1:i, 1:]
+				diff_frame.append(diff[0])
 
-	diff_frame = list()
-	for i in range(len(full_frame)):
-		if i==0: diff_frame.append(full_frame[:1, 1:][0])
-		else:
-			diff = full_frame[i:i+1, 1:] - full_frame[i-1:i, 1:]
-			diff_frame.append(diff[0])
-	diff_frame = np.array(diff_frame)
+		diff_frame = np.array(diff_frame)
+		print("diff_frame created with shape", diff_frame.shape)
 
-	print("diff_frame created with shape", diff_frame.shape)
+		# Generating set2 col name
+		df2 = pd.DataFrame.from_records(diff_frame, columns = [str("d" + str(metric_no) + names) for names in col_names])
+		print("df2 created !!!!")
 
-
-	# Generating set2 col name
+		result_df = pd.concat([df1, df2], axis=1)
+		outputFileName = "Metric_" + str(metric_no) + "_36.pkl"
+	else:
+		result_df = df1
+		outputFileName = "Metric_" + str(metric_no) + "_18.pkl"
 	
-	df2 = pd.DataFrame.from_records(diff_frame, columns = [str("d"+metric_no+names) for names in col_names])
-	print("df2 created !!!!")
-
-
-	result_df = pd.concat([df1, df2], axis=1)
 	print(result_df.shape)
 
 	# Pickle this data
-
-	outfile = open("Metric_"+str(metric_no)+"_36.pkl",'wb')
+	outfile = open(outputFileName, 'wb')
 	pickle.dump(result_df, outfile)
 	outfile.close()
-	return "Metric_"+str(metric_no)+"_36.pkl"
-
-
-
+	return outputFileName
 
 
 if __name__ == "__main__":
-	pids = seperate_pid_data()
-	features, pid = ["Mean", "Median", "Std_dev"], pids[0]
-	for i, fea in enumerat(features):
-		filename = create_per_second_data(pid, fea)
+	# pids = seperate_pid_data()
+	features, pid = ["Mean", "Median", "Std_Dev", "ZeroCrsRate", "Max_Raw", "Min_Raw", "Max_Abs", "Min_Abs",
+	"Spec_Ent_Time", "Spec_Ent_Freq", "Spec_Cent", "Spec_Sprd", "Spec_Flux", "Spec_RollOff", "Max_freq",
+	"Skewness", "Kurtosis", "Avg_Power"], 'BK7610'
+	for i,_ in enumerate(features):
+		if i == 12: continue
+		filename = create_per_second_data(pid, i)
 		filename = create_per_window_data(filename, i)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ utility.py
@@ -0,0 +1,133 @@
+import numpy as np
+from scipy.stats import skew, kurtosis
+from scipy.signal import welch
+
+eps = 0.00000001
+
+# Computes zero crossing rate of frame
+def zero_crossing_rate(frame):
+    count = len(frame)
+    count_zero = np.sum(np.abs(np.diff(np.sign(frame)))) / 2
+    return np.float64(count_zero) / np.float64(count - 1.0)
+
+
+# Computes the spectral entropy (time and frequency)
+def spectral_entropy(signal, n_short_blocks=10):
+    # number of frame samples
+    num_frames = len(signal)
+
+    # total spectral energy
+    total_energy = np.sum(signal ** 2)
+
+    # length of sub-frame
+    sub_win_len = int(np.floor(num_frames / n_short_blocks))
+    if num_frames != sub_win_len * n_short_blocks:
+        signal = signal[0:sub_win_len * n_short_blocks]
+
+    # define sub-frames (using matrix reshape)
+    sub_wins = signal.reshape(sub_win_len, n_short_blocks, order='F').copy()
+
+    # compute spectral sub-energies
+    s = np.sum(sub_wins ** 2, axis=0) / (total_energy + eps)
+
+    # compute spectral entropy
+    entropy = -np.sum(s * np.log2(s + eps))
+
+    return entropy
+
+
+# Computes spectral centroid of frame (given abs(FFT))
+def spectral_centroid(fft_magnitude, sampling_rate=40):
+    ind = (np.arange(1, len(fft_magnitude) + 1)) * (sampling_rate / (2.0 * len(fft_magnitude)))
+
+    Xt = fft_magnitude.copy()
+    Xt = Xt / Xt.max()
+    NUM = np.sum(ind * Xt)
+    DEN = np.sum(Xt) + eps
+
+    # Centroid:
+    centroid = (NUM / DEN)
+
+    # Normalize:
+    centroid = centroid / (sampling_rate / 2.0)
+
+    return centroid
+
+
+# Computes spectral centroid of frame (given abs(FFT))
+def spectral_spread(fft_magnitude, sampling_rate=40):
+    ind = (np.arange(1, len(fft_magnitude) + 1)) * (sampling_rate / (2.0 * len(fft_magnitude)))
+
+    Xt = fft_magnitude.copy()
+    Xt = Xt / Xt.max()
+    NUM = np.sum(ind * Xt)
+    DEN = np.sum(Xt) + eps
+
+    # Spread:
+    spread = np.sqrt(np.sum(((ind - (NUM / DEN)) ** 2) * Xt) / DEN)
+
+    # Normalize:
+    spread = spread / (sampling_rate / 2.0)
+
+    return spread
+
+
+# Computes the spectral flux feature of the current frame
+def spectral_flux(fft_magnitude, previous_fft_magnitude):
+    # compute the spectral flux as the sum of square distances:
+    fft_sum = np.sum(fft_magnitude + eps)
+    previous_fft_sum = np.sum(previous_fft_magnitude + eps)
+    sp_flux = np.sum((fft_magnitude / fft_sum - previous_fft_magnitude / previous_fft_sum) ** 2)
+
+    return sp_flux
+
+
+# Computes spectral roll-off
+def spectral_rolloff(signal, c=0.90):
+    energy = np.sum(signal ** 2)
+    fft_length = len(signal)
+    threshold = c * energy
+    # Find the spectral rolloff as the frequency position where the respective spectral energy is equal to c*totalEnergy
+    cumulative_sum = np.cumsum(signal ** 2) + eps
+    a = np.nonzero(cumulative_sum > threshold)[0]
+    sp_rolloff = 0.0
+    if len(a) > 0: sp_rolloff = np.float64(a[0]) / (float(fft_length))
+
+    return sp_rolloff
+
+
+def skewness(signal):
+    return skew(signal)
+
+
+def avg_power(signal):
+    return np.mean(welch(signal, 40))
+
+
+def rms(signal):
+    return np.sqrt(np.mean(signal ** 2))
+
+
+# Features
+#1  Mean
+#2  Standard Deviation
+#3  Median
+#4  Zero Crossing Rate
+#5  Max/Min, Raw/Abs
+#6  Spectral Entropy
+#7  Spectral Centroid
+#8  Spectral Spread
+#10 Spectral Roll-off
+#11 Max Frequency
+#16 Skewness
+#17 Kurtosis
+#18 Average Power
+
+#9  Spectral Flux
+#12 Gait Stretch
+#13 Number of Steps
+#14 Step Time
+#15 Cadence
+#19 Spectral Peak Ratio
+#20 RMS
+#21 MFCC Covariance
